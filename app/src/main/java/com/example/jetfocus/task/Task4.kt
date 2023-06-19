71794a6c0b6d041aebd58128ff18940b4696a92a
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-20 01:19:51 +0800

    Task 4 answer is completed.

--- app/src/main/java/com/example/jetfocus/task/Task4.kt
+++ app/src/main/java/com/example/jetfocus/task/Task4.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -12,16 +13,20 @@ import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
 
 class Task4 {
-    // 把文字水平对齐
+    // 把视图水平对齐
     @Composable
     fun Card() {
-        Column {
+        Column(
+             horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             Image(
                 modifier = Modifier.size(96.dp),
                 painter = painterResource(R.drawable.jetpack_compose),
                 contentDescription = null
             )
-            Text("Hello World")
+            Text(
+                text = "Hello World",
+            )
         }
     }
 
