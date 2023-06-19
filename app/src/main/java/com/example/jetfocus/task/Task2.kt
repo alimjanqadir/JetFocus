cbef1e47430bde76f92a1ff003d1f72555748b1b
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-19 23:41:05 +0800

    Task 2 is answer is completed.

--- app/src/main/java/com/example/jetfocus/task/Task2.kt
+++ app/src/main/java/com/example/jetfocus/task/Task2.kt
@@ -1,13 +1,23 @@
 package com.example.jetfocus.task
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetfocus.R
 
 // 在原有的基础上文字上面再加一个图片，图片ID为R.drawable.jetpack_compose
 @Composable
 fun Card() {
-    Text("Hello World")
+    Column {
+        Image(
+            painter = painterResource(R.drawable.jetpack_compose),
+            contentDescription = null
+        )
+        Text("Hello World")
+    }
 }
 
 @Composable
