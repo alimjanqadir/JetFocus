8fce01cc70d90ec1e75df642420ee021550165d6
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-19 22:45:12 +0800

    Task 1 answer is completed.

--- app/src/main/java/com/example/jetfocus/task/Task1.kt
+++ app/src/main/java/com/example/jetfocus/task/Task1.kt
@@ -1,3 +1,17 @@
 package com.example.jetfocus.task
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
 // 写一个HelloWorld的Composable函数并绘制一个“Hello Jetpack Compose!”的文字。
+@Composable
+fun HelloWorld() {
+    Text("Hello World")
+}
+
+@Composable
+@Preview()
+fun Preview() {
+    HelloWorld()
+}
