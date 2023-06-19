e8f95a21ac90569f381f3cfb19e4d0ec45d5498c
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-20 00:16:42 +0800

    Task 3 answer is completed.

--- app/src/main/java/com/example/jetfocus/task/Task3.kt
+++ app/src/main/java/com/example/jetfocus/task/Task3.kt
@@ -2,18 +2,22 @@ package com.example.jetfocus.task
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
 
-class Task2 {
+class Task3 {
     // 改变图像的大小到96dp
     @Composable
     fun Card() {
         Column {
             Image(
+                modifier = Modifier.size(96.dp),
                 painter = painterResource(R.drawable.jetpack_compose),
                 contentDescription = null
             )
