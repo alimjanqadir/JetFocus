cd3d79265eb2de4606df706bac92157dc1f135f4
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-20 12:34:19 +0800

    Theme created for Task 6.

---
+++ app/src/main/java/com/example/jetfocus/ui/theme/Theme.kt
@@ -0,0 +1,31 @@
+package com.example.jetfocus.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColors = lightColorScheme(
+    primary = lightPrimary,
+    onPrimary = lightOnPrimary,
+    surface = lightSurface,
+    onSurface = lightOnSurface
+)
+
+private val DarkColors = lightColorScheme(
+    primary = darkPrimary,
+    onPrimary = darkOnPrimary,
+    surface = darkSurface,
+    onSurface = darkOnSurface
+)
+
+@Composable
+fun TaskTheme(
+    useDarkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit
+) {
+    val colors = if (useDarkTheme) DarkColors else LightColors
+    MaterialTheme(
+        colorScheme = colors,
+        content = content
+    )
+}
