1eeb84382eb8d6e29e18451eb0675050f807c6be
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-21 04:28:28 +0800

    Task1 definition updated.

--- app/src/main/java/com/example/jetfocus/task/Task1.kt
+++ app/src/main/java/com/example/jetfocus/task/Task1.kt
@@ -1,6 +1,19 @@
 package com.example.jetfocus.task
 
-// 写一个HelloWorld的Composable函数并绘制一个“Hello Jetpack Compose!”的文字。
-class Task1 {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
+/* 写出你在这次活动的第一个Composable函数，写完以后删除注释可以看到结果 */
+class Task1 {
+    @Composable
+    fun HelloWorld() {
+        Text("Hello Jetpack Compose!")
+    }
+
+    @Preview
+    @Composable
+    fun Preview() {
+        HelloWorld()
+    }
 }
