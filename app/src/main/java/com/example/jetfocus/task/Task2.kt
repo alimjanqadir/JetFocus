3b3df65248389f1af12efdc9b9ea149d8944a432
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-21 04:36:44 +0800

    Task2 definition is updated.

--- app/src/main/java/com/example/jetfocus/task/Task2.kt
+++ app/src/main/java/com/example/jetfocus/task/Task2.kt
@@ -4,16 +4,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
+/*熟悉布局：Column。*/
 class Task2 {
-    // 在原有的基础上文字上面再加一个图片，图片ID为R.drawable.jetpack_compose
+      // TODO("1. 把“Hello World”的文字换成“Andy Holloway”的姓名")
+      // TODO("2. 然后文字上方加一个图片，用已有的资源R.drawable.andy")
+      // TODO("3. 最后把HelloWorld的函数名换成UserInfoCard")
+
     @Composable
-    fun Card() {
-        Text("Hello World")
+    fun HelloWorld() {
+        Text("Hello Jetpack Compose!")
     }
 
     @Composable
     @Preview()
     fun Preview() {
-        Card()
+        //UserInfoCard()
     }
 }
