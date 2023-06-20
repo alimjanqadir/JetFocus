414e758ae47bfc652f4e25b947bdfc88971c8200
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-21 07:19:52 +0800

    Task 4 definition is updated.

--- app/src/main/java/com/example/jetfocus/task/Task4.kt
+++ app/src/main/java/com/example/jetfocus/task/Task4.kt
@@ -2,32 +2,37 @@ package com.example.jetfocus.task
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
 
+// Modifiers。
 class Task4 {
-    // 把文字水平对齐
+    // TODO(调整各个视图的大小，对齐，左右上下空间使界面更加整齐，然后把图片改成圆角。)
     @Composable
-    fun Card() {
-        Column {
-            Image(
-                modifier = Modifier.size(96.dp),
-                painter = painterResource(R.drawable.jetpack_compose),
-                contentDescription = null
-            )
-            Text("Hello World")
+    fun UserInfoCard() {
+        Row {
+            Column {
+                Image(
+                    painter = painterResource(R.drawable.andy), contentDescription = null
+                )
+                Text("Andy Holloway")
+            }
+            Column {
+                Text("Android Developer")
+                Text("Andy 是相当有经验的安卓开发者，他参与了Google的各类大型项目，他喜欢游泳和烘培。")
+            }
         }
     }
 
     @Composable
-    @Preview()
+    @Preview
     fun Preview() {
-        Card()
+        UserInfoCard()
     }
 }
+
+
