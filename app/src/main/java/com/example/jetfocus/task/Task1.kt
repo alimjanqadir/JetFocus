a5e5b64131560008159ca42484ef66f685ac97d5
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-21 04:25:10 +0800

    Answer updated.

--- app/src/main/java/com/example/jetfocus/task/Task1.kt
+++ app/src/main/java/com/example/jetfocus/task/Task1.kt
@@ -4,14 +4,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
-// 写一个HelloWorld的Composable函数并绘制一个“Hello Jetpack Compose!”的文字。
-@Composable
-fun HelloWorld() {
-    Text("Hello World")
-}
+/* 写出你在这次活动的第一个Composable函数，写完以后删除注释可以看到结果 */
+class Task1 {
+    @Composable
+    fun HelloWorld() {
+        Text("Hello Jetpack Compose!")
+    }
 
-@Composable
-@Preview()
-fun Preview() {
-    HelloWorld()
+    @Preview
+    @Composable
+    fun Preview() {
+        HelloWorld()
+    }
 }
