f123cb5383eae636561a030b968221e7773b2011
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-20 12:34:49 +0800

    Task 6 is definded.

---
+++ app/src/main/java/com/example/jetfocus/task/Task6.kt
@@ -0,0 +1,56 @@
+package com.example.jetfocus.task
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetfocus.R
+import com.example.jetfocus.ui.theme.TaskTheme
+
+class Task6 {
+    // 把Column包含在TaskTheme和Surface里面
+    @Composable
+    fun UI() {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(96.dp)
+                    .clip(CircleShape),
+                painter = painterResource(R.drawable.jetpack_compose),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Hello World",
+            )
+        }
+    }
+
+    @Composable
+    @Preview(
+        name = "Light Mode",
+    )
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        name = "Dark Mode",
+    )
+    fun Preview() {
+        UI()
+    }
+}
