7573f0b28c774fad01eb5b2691786741459135ee
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-21 07:34:24 +0800

    Task 5 answer is updated.

--- app/src/main/java/com/example/jetfocus/task/Task5.kt
+++ app/src/main/java/com/example/jetfocus/task/Task5.kt
@@ -1,12 +1,17 @@
 package com.example.jetfocus.task
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,30 +21,43 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
+import com.example.jetfocus.ui.theme.TaskTheme
 
+// 主题（皮肤或Theming）
 class Task5 {
-    // 把图片圆边和为个视图加一下空间
     @Composable
-    fun Card() {
-        Column(
-            modifier = Modifier.padding(8.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                modifier = Modifier
-                    .size(96.dp)
-                    .clip(CircleShape),
-                painter = painterResource(R.drawable.jetpack_compose),
-                contentDescription = null
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text("Hello World")
+    fun UserInfoCard() {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier.padding(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(56.dp)
+                        .clip(CircleShape),
+                    painter = painterResource(R.drawable.andy),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("Andy Holloway")
+            }
+            Column(modifier = Modifier.padding(vertical = 12.dp)) {
+                Text("Android Developer")
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Andy 是相当有经验的安卓开发者，他参与了Google的各类大型项目，他喜欢游泳和烘培。")
+            }
         }
     }
 
     @Composable
-    @Preview()
+    @Preview(showSystemUi = true)
+    @Preview(showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
     fun Preview() {
-        Card()
+        TaskTheme {
+            Surface {
+                UserInfoCard()
+            }
+        }
     }
 }
