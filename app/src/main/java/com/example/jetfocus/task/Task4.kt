13f370ee51a4161fb8a1df5b28937e929ab6e946
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-21 07:14:32 +0800

    Task 4 answer is updated.

--- app/src/main/java/com/example/jetfocus/task/Task4.kt
+++ app/src/main/java/com/example/jetfocus/task/Task4.kt
@@ -2,37 +2,55 @@ package com.example.jetfocus.task
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
 
+// Modifiers。
 class Task4 {
-    // 把视图水平对齐
+    // TODO(调整各个视图的大小，对齐，左右上下空间使界面更加整齐，然后把图片改成圆角。)
     @Composable
-    fun Card() {
-        Column(
-             horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                modifier = Modifier.size(96.dp),
-                painter = painterResource(R.drawable.jetpack_compose),
-                contentDescription = null
-            )
-            Text(
-                text = "Hello World",
-            )
+    fun UserInfoCard() {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier.padding(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(56.dp)
+                        .clip(CircleShape),
+                    painter = painterResource(R.drawable.andy),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("Andy Holloway")
+            }
+            Column(modifier = Modifier.padding(vertical = 12.dp)) {
+                Text("Android Developer")
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Andy 是相当有经验的安卓开发者，他参与了Google的各类大型项目，他喜欢游泳和烘培。")
+            }
         }
     }
 
     @Composable
-    @Preview()
+    @Preview(showSystemUi = true)
     fun Preview() {
-        Card()
+        UserInfoCard()
     }
 }
