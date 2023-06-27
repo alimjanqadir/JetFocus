14ef6efd23f33fc68be1531a0265e659176700bc
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-27 11:00:37 +0800

    Task 6 answer is updated.

--- app/src/main/java/com/example/jetfocus/task/Task6.kt
+++ app/src/main/java/com/example/jetfocus/task/Task6.kt
@@ -1,13 +1,18 @@
 package com.example.jetfocus.task
 
 import android.content.res.Configuration
+import android.provider.Contacts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +25,39 @@ import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
 import com.example.jetfocus.ui.theme.TaskTheme
 
+/*显示大量数据*/
 class Task6 {
-    // 把Column包含在TaskTheme和Surface里面
     @Composable
     fun UI() {
-        TaskTheme {
-            Surface {
-                Column(
-                    modifier = Modifier.padding(8.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(
-                        modifier = Modifier
-                            .size(96.dp)
-                            .clip(CircleShape),
-                        painter = painterResource(R.drawable.jetpack_compose),
-                        contentDescription = null
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(
-                        text = "Hello World",
-                    )
-                }
+        val scrollState = rememberScrollState()
+        Column {
+            repeat(100) {
+                UserInfoCard()
+            }
+        }
+    }
+
+    @Composable
+    fun UserInfoCard() {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier.padding(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(56.dp)
+                        .clip(CircleShape),
+                    painter = painterResource(R.drawable.andy),
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("Andy Holloway")
+            }
+            Column(modifier = Modifier.padding(vertical = 12.dp)) {
+                Text("Android Developer")
+                Spacer(modifier = Modifier.height(10.dp))
+                Text("Andy 是相当有经验的安卓开发者，他参与了Google的各类大型项目，他喜欢游泳和烘培。")
             }
         }
     }
@@ -55,6 +71,10 @@ class Task6 {
         name = "Dark Mode",
     )
     fun Preview() {
-        UI()
+        TaskTheme {
+            Surface {
+                UI()
+            }
+        }
     }
 }
