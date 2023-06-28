ac7773946b16091a046a133417a319bdeacaea22
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-28 17:04:29 +0800

    Implements task 10 answer.

--- app/src/main/java/com/example/jetfocus/task/Task10.kt
+++ app/src/main/java/com/example/jetfocus/task/Task10.kt
@@ -1,18 +1,23 @@
 package com.example.jetfocus.task
 
 import android.content.res.Configuration
+import android.widget.CheckBox
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetfocus.ui.theme.TaskTheme
 
 // 重组和状态
 class Task10 {
-    // TODO("显示一个CheckBox组件")
-    // TODO("为什么对组件的操作没有任何效果")
     @Composable
     fun UI() {
-
+        var isChecked by remember { mutableStateOf(false) }
+        Checkbox(checked = isChecked, onCheckedChange = { isChecked = !isChecked })
     }
 
     @Composable
