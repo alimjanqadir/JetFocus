4670f1f275b9e0585673b2d8153258a1a3157918
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-28 16:42:45 +0800

    Implements task 9 answer which including compressing images and updating sample data.

--- app/src/main/java/com/example/jetfocus/task/SampleData.kt
+++ app/src/main/java/com/example/jetfocus/task/SampleData.kt
@@ -5,5 +5,14 @@ import com.example.jetfocus.R
 data class User(val imageId: Int, val name: String, val title: String, val intro: String)
 
 val users = listOf(
-    User(R.drawable.jetpack_compose, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_1, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_2, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_3, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_4, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_5, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_6, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_7, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_8, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_9, "Andy", "Programmer", "Test"),
+    User(R.drawable.avatar_10, "Andy", "Programmer", "Test"),
 )
--- app/src/main/java/com/example/jetfocus/task/Task9.kt
+++ app/src/main/java/com/example/jetfocus/task/Task9.kt
@@ -1,36 +1,39 @@
 package com.example.jetfocus.task
 
 import android.content.res.Configuration
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetfocus.R
 import com.example.jetfocus.ui.theme.TaskTheme
 
 // Grids
+@OptIn(ExperimentalFoundationApi::class)
 class Task9 {
-    // TODO("用LazyVerticalGrid实现网格性数据显示")
+    // TODO("用LazyVerticalGrid实现3*3的网格数据显示")
     // TODO("如果用LazyHorizontalGrid会有什么不一样")
     // TODO("也试试用LazyVerticalStaggeredGrid和LazyHorizontalStaggeredGrid。")
     @Composable
     fun UI() {
-
+        LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+            users.forEach {user ->
+                item {
+                    UserAvatar(user.imageId)
+                }
+            }
+        }
     }
 
     @Composable
-    fun UserInfoCard() {
-        Column {
-            Image(
-                painter = painterResource(R.drawable.andy),
-                contentDescription = null
-            )
-            Text("Andy Holloway")
-        }
+    fun UserAvatar(imageId: Int) {
+        Image(
+            painter = painterResource(imageId),
+            contentDescription = null
+        )
     }
 
     @Composable
