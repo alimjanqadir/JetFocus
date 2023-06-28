c409fa6ae293a5485ee954584e85af88ac4cae0e
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-06-28 14:37:59 +0800

    Updates task 9 definition.

--- app/src/main/java/com/example/jetfocus/task/Task9.kt
+++ app/src/main/java/com/example/jetfocus/task/Task9.kt
@@ -1,5 +1,51 @@
 package com.example.jetfocus.task
 
-// LazyStaggeredGrid
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetfocus.R
+import com.example.jetfocus.ui.theme.TaskTheme
+
+// Grids
 class Task9 {
+    // TODO("用LazyVerticalGrid实现网格性数据显示")
+    // TODO("如果用LazyHorizontalGrid会有什么不一样")
+    // TODO("也试试用LazyVerticalStaggeredGrid和LazyHorizontalStaggeredGrid。")
+    @Composable
+    fun UI() {
+
+    }
+
+    @Composable
+    fun UserInfoCard() {
+        Column {
+            Image(
+                painter = painterResource(R.drawable.andy),
+                contentDescription = null
+            )
+            Text("Andy Holloway")
+        }
+    }
+
+    @Composable
+    @Preview(
+        name = "Light Mode",
+    )
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        name = "Dark Mode",
+    )
+    fun Preview() {
+        TaskTheme {
+            Surface {
+                UI()
+            }
+        }
+    }
 }
