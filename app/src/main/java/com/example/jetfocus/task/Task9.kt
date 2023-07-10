de836bdb128c2a5dd8b4d3585d72a2657ff24509
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-07-10 15:07:06 +0800

    Updates task 9 answer.

--- app/src/main/java/com/example/jetfocus/task/Task9.kt
+++ app/src/main/java/com/example/jetfocus/task/Task9.kt
@@ -1,37 +1,106 @@
+@file:Suppress("unused")
+
 package com.example.jetfocus.task
 
 import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetfocus.ui.theme.TaskTheme
 
 // Grids
 @OptIn(ExperimentalFoundationApi::class)
 class Task9 {
-    // TODO("用LazyVerticalGrid实现3*3的网格数据显示")
-    // TODO("如果用LazyHorizontalGrid会有什么不一样")
-    // TODO("也试试用LazyVerticalStaggeredGrid和LazyHorizontalStaggeredGrid。")
     @Composable
     fun UI() {
         LazyVerticalGrid(columns = GridCells.Fixed(3)) {
-            users.forEach {user ->
+            users.forEach { user ->
                 item {
-                    UserAvatar(user.imageId)
+                    UserAvatar(user)
                 }
             }
         }
     }
 
     @Composable
-    fun UserAvatar(imageId: Int) {
+    fun UI1() {
+        LazyVerticalGrid(columns = GridCells.Adaptive(80.dp)) {
+            users.forEach { user ->
+                item {
+                    UserAvatar(user)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun UI2() {
+        LazyVerticalGrid(columns = GridCells.Fixed(3)) {
+            users.forEach { user ->
+                item {
+                    UserAvatar(
+                        modifier = Modifier.size(80.dp),
+                        contentScale = ContentScale.Crop,
+                        user = user
+                    )
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun UI3() {
+        LazyVerticalStaggeredGrid(columns = StaggeredGridCells.Fixed(3)) {
+            users.forEach { user ->
+                item {
+                    UserAvatar(user)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun UI4() {
+        LazyVerticalStaggeredGrid(columns = StaggeredGridCells.Fixed(3)) {
+            users.forEach { user ->
+                item {
+                    UserAvatar(user, contentScale = ContentScale.FillWidth)
+                }
+            }
+        }
+    }
+
+
+    @Composable
+    fun UI5() {
+        LazyVerticalStaggeredGrid(columns = StaggeredGridCells.Adaptive(80.dp)) {
+            users.forEach { user ->
+                item {
+                    UserAvatar(user)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun UserAvatar(
+        user: User, modifier: Modifier = Modifier, contentScale: ContentScale = ContentScale.Fit
+    ) {
         Image(
-            painter = painterResource(imageId),
+            modifier = modifier,
+            contentScale = contentScale,
+            painter = painterResource(user.imageId),
             contentDescription = null
         )
     }
@@ -43,6 +112,14 @@ class Task9 {
     @Preview(
         uiMode = Configuration.UI_MODE_NIGHT_YES,
         name = "Dark Mode",
+    )
+    @Preview(
+        name = "Tablet",
+        device = "spec:width=1280dp,height=800dp,dpi=480,orientation=portrait",
+    )
+    @Preview(
+        name = "Tablet",
+        device = "spec:width=1280dp,height=800dp,dpi=480",
     )
     fun Preview() {
         TaskTheme {
