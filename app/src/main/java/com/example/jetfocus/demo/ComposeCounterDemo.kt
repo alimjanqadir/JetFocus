154d513980a82af4427e0d76a8c8f6cf3633edac
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-07-14 23:24:38 +0800

    Adds compose counter demo.

---
+++ app/src/main/java/com/example/jetfocus/demo/ComposeCounterDemo.kt
@@ -0,0 +1,34 @@
+package com.example.jetfocus.demo
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+//TODO(Theme, DynamicColor, Preview)
+
+@Preview
+@Composable
+fun Counter() {
+    var count by remember { mutableStateOf(0) }
+    Row {
+        Button(onClick = { count += 1 }) {
+            Text("+")
+        }
+        Text(
+            text = "$count", fontSize = 24.sp, modifier = Modifier.padding(8.dp)
+        )
+        Button(onClick = { count -= 1 }) {
+            Text("-")
+        }
+    }
+}
