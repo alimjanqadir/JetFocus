14cb50e14dde06b6f27f8c03022acd92ed8532dc
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-07-17 11:41:42 +0800

    Creates a demo for statefull widgets.

---
+++ app/src/main/java/com/example/jetfocus/demo/OtherControls.kt
@@ -0,0 +1,16 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.jetfocus.demo
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+// TODO(TextField, mutableStateOf and remember, Checkbox, Switch, AlertDialog)
+
+@Preview
+@Composable
+fun OtherControlsDemo() {
+}
+
+
