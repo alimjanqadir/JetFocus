5d5b309f0c82fd50de3f9ff672254700763ed1cf
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-07-17 11:39:31 +0800

    Updates image demo definition.

--- app/src/main/java/com/example/jetfocus/demo/ImageDemo.kt
+++ app/src/main/java/com/example/jetfocus/demo/ImageDemo.kt
@@ -1,17 +1,10 @@
 package com.example.jetfocus.demo
 
-import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetfocus.R
 
-// TODO("显示一个图片，没有大小，或其他修改")
+// TODO("显示一个图片用资源‘R.drawable.jetpack_compose’", modifier: size, contentScale, padding, clip, border, clickable)
 @Preview
 @Composable
 fun ImageDemo() {
-    Image(
-        painter = painterResource(R.drawable.jetpack_compose),
-        contentDescription = null
-    )
 }
