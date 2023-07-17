3f154f352bfc05eb40f46403f978c8fe3c1c4d91
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-07-17 11:37:34 +0800

    Updates text demo definition.

--- app/src/main/java/com/example/jetfocus/demo/TextDemo.kt
+++ app/src/main/java/com/example/jetfocus/demo/TextDemo.kt
@@ -1,17 +1,11 @@
 package com.example.jetfocus.demo
 
-import androidx.compose.foundation.Image
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetfocus.R
 
-// TODO("显示一个文字，内容为Hello World")
+// TODO(@Composable, text, color, fontStyle, fontSize, fontFamily)
+
 @Preview
 @Composable
 fun TextDemo() {
-    Text(
-        "Hello World"
-    )
 }
