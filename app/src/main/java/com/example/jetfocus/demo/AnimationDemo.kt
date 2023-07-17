865e0c931c797f1b8cb12a9005b20d8b6dffdbc0
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-07-17 12:21:37 +0800

    Updates animation demo.

--- app/src/main/java/com/example/jetfocus/demo/AnimationDemo.kt
+++ app/src/main/java/com/example/jetfocus/demo/AnimationDemo.kt
@@ -1,31 +1,42 @@
+@file:OptIn(ExperimentalAnimationApi::class)
+
 package com.example.jetfocus.demo
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 fun AnimationDemo() {
-    var visible by remember { mutableStateOf(false) }
+    val visible by remember { mutableStateOf(true) }
     Column {
-        Button(onClick = { visible = !visible }) {
-            if (visible) {
-                Text("Hide")
-            } else {
-                Text("Show")
-            }
-        }
-        AnimatedVisibility(visible) {
-            Text("Hello")
+        val density = LocalDensity.current
+        AnimatedVisibility(
+            visible = visible,
+            enter = slideInVertically { with(density) { -40.dp.roundToPx() } }
+                    + expandVertically(expandFrom = Alignment.Top)
+                    + fadeIn(initialAlpha = 0.3f),
+            exit = slideOutVertically() + shrinkVertically() + fadeOut()
+        ) {
+            Text(
+                "Hello",
+            )
         }
     }
 }
