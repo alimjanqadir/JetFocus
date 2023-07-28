be110ce00184d12fbe14596805fcaa604d596418
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-07-28 23:52:19 +0800

    Updates canvas demo.

--- app/src/main/java/com/example/jetfocus/demo/event1/CustomViewComponent.kt
+++ app/src/main/java/com/example/jetfocus/demo/event1/CustomViewComponent.kt
@@ -1,59 +1,15 @@
 package com.example.jetfocus.demo.event1
 
 import android.content.Context
+import android.content.res.Resources.getSystem
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.drawable.Drawable
-import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
 import com.example.jetfocus.R
 
 class CustomViewComponent : View {
-
-    private var _exampleString: String? = null // TODO: use a default from R.string...
-    private var _exampleColor: Int = Color.RED // TODO: use a default from R.color...
-    private var _exampleDimension: Float = 0f // TODO: use a default from R.dimen...
-
-    private lateinit var textPaint: TextPaint
-    private var textWidth: Float = 0f
-    private var textHeight: Float = 0f
-
-    /**
-     * The text to draw
-     */
-    var exampleString: String?
-        get() = _exampleString
-        set(value) {
-            _exampleString = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * The font color
-     */
-    var exampleColor: Int
-        get() = _exampleColor
-        set(value) {
-            _exampleColor = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * In the example view, this dimension is the font size.
-     */
-    var exampleDimension: Float
-        get() = _exampleDimension
-        set(value) {
-            _exampleDimension = value
-            invalidateTextPaintAndMeasurements()
-        }
-
-    /**
-     * In the example view, this drawable is drawn above the text.
-     */
-    var exampleDrawable: Drawable? = null
 
     constructor(context: Context) : super(context) {
         init(null, 0)
@@ -76,79 +32,38 @@ class CustomViewComponent : View {
         val a = context.obtainStyledAttributes(
             attrs, R.styleable.CustomViewComponent, defStyle, 0
         )
-
-        _exampleString = a.getString(
-            R.styleable.CustomViewComponent_exampleString
-        )
-        _exampleColor = a.getColor(
-            R.styleable.CustomViewComponent_exampleColor,
-            exampleColor
-        )
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        _exampleDimension = a.getDimension(
-            R.styleable.CustomViewComponent_exampleDimension,
-            exampleDimension
-        )
-
-        if (a.hasValue(R.styleable.CustomViewComponent_exampleDrawable)) {
-            exampleDrawable = a.getDrawable(
-                R.styleable.CustomViewComponent_exampleDrawable
-            )
-            exampleDrawable?.callback = this
-        }
-
         a.recycle()
 
         // Set up a default TextPaint object
-        textPaint = TextPaint().apply {
-            flags = Paint.ANTI_ALIAS_FLAG
-            textAlign = Paint.Align.LEFT
-        }
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements()
+        paint = Paint()
     }
 
-    private fun invalidateTextPaintAndMeasurements() {
-        textPaint.let {
-            it.textSize = exampleDimension
-            it.color = exampleColor
-            textWidth = it.measureText(exampleString)
-            textHeight = it.fontMetrics.bottom
-        }
-    }
 
+    private lateinit var paint: Paint
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        val density = getSystem().displayMetrics.density
+        val radius = 100f * density
+        val centerX = width / 2f
+        val centerY = height / 2f
 
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        val paddingLeft = paddingLeft
-        val paddingTop = paddingTop
-        val paddingRight = paddingRight
-        val paddingBottom = paddingBottom
+        paint.color = Color.RED
+        canvas.drawCircle(centerX, centerY, radius, paint)
+        paint.color = Color.GREEN
+        canvas.drawCircle(centerX, centerY, radius * 0.8f, paint)
+        paint.color = Color.BLUE
+        canvas.drawCircle(centerX, centerY, radius * 0.6f, paint)
 
-        val contentWidth = width - paddingLeft - paddingRight
-        val contentHeight = height - paddingTop - paddingBottom
-
-        exampleString?.let {
-            // Draw the text.
-            canvas.drawText(
-                it,
-                paddingLeft + (contentWidth - textWidth) / 2,
-                paddingTop + (contentHeight + textHeight) / 2,
-                textPaint
-            )
-        }
-
-        // Draw the example drawable on top of the text.
-        exampleDrawable?.let {
-            it.setBounds(
-                paddingLeft, paddingTop,
-                paddingLeft + contentWidth, paddingTop + contentHeight
-            )
-            it.draw(canvas)
-        }
+        paint.color = Color.BLACK
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 10f * density
+        canvas.drawRect(
+            centerX - (250f * density) / 2,
+            centerY - (300f * density) / 2 ,
+            centerX + (250f * density) / 2,
+            centerY + (300f * density) / 2,
+            paint
+        )
+        paint.style = Paint.Style.FILL
     }
 }
--- app/src/main/java/com/example/jetfocus/demo/event2/CanvasDemo.kt
+++ app/src/main/java/com/example/jetfocus/demo/event2/CanvasDemo.kt
@@ -4,13 +4,28 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
 @Composable
 fun CanvasDemo() {
     Canvas(modifier = Modifier.fillMaxSize()) {
-        drawCircle(Color.Black)
+        drawCircle(Color.Red, radius = 100.dp.toPx())
+        drawCircle(Color.Green, radius = 80.dp.toPx())
+        drawCircle(Color.Blue, radius = 60.dp.toPx())
+        drawRect(
+            Color.Black,
+            topLeft = Offset(
+                center.x - (250.dp / 2).toPx(),
+                center.y - (300.dp / 2).toPx()
+            ),
+            size = Size(250.dp.toPx(), 300.dp.toPx()),
+            style = Stroke(10.dp.toPx())
+        )
     }
 }
