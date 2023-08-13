7397ac2d9de5a39d7e73a4e6d2ad01c0acdec056
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2023-08-13 12:22:02 +0800

    Updates Curve task.

--- app/src/main/java/com/example/jetfocus/task/CurveTask.kt
+++ app/src/main/java/com/example/jetfocus/task/CurveTask.kt
@@ -8,17 +8,25 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.text.ExperimentalTextApi
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
+import graphics.drawGrid
+import graphics.drawRuler
 
-@Preview
+@OptIn(ExperimentalTextApi::class)
+@Preview(device = "spec:width=250dp,height=250dp,dpi=640")
 @Composable
 fun CurveTask() {
+    val textMeasurer = rememberTextMeasurer()
     Box(modifier = Modifier
         .fillMaxSize()
         .background(Color.White)
         .drawWithCache {
             val path = Path()
             onDrawBehind {
+                drawGrid()
+                drawRuler(textMeasurer)
             }
         })
 }
